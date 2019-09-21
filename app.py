09a81e78f058ee988fb0754f16ff08d12128acfc
jrandiny <jrandiny2@gmail.com>
2019-09-21 15:57:47 +0700

    Add cli input

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import threading
 from network import listener, send
 
 if __name__ == "__main__":
-    port = int(input("Port : "))
+    port = int(input("Input listener port : "))
     thread_quit = threading.Event()
 
     network_thread = threading.Thread(target=listener,
@@ -10,20 +10,16 @@ if __name__ == "__main__":
     network_thread.start()
 
     while True:
-        input("aa")
-        with open("util.py", "rb") as binary_file:
-            send(binary_file.read(), "127.0.0.1", port)
-        input()
-        # user_input = input("> ")
-        # input_list = user_input.split(" ")
+        user_input = input("> ")
+        input_list = user_input.split(" ")
 
-        # if (input_list[0] == "send"):
-        #     with open(input_list[1], "rb") as binary_file:
-        #         send(binary_file.read(), input_list[2], port)
-        # elif (input_list[0] == "quit"):
-        #     break
-        # else:
-        #     print("Invalid input")
+        if (input_list[0] == "send"):
+            with open(input_list[1], "rb") as binary_file:
+                send(binary_file.read(), input_list[2], port)
+        elif (input_list[0] == "quit"):
+            break
+        else:
+            print("Invalid input")
 
     thread_quit.set()
     network_thread.join()
