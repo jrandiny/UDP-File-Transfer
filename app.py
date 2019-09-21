c4d61b3e49f604445b3ca58bf76254b6be229298
jrandiny <jrandiny2@gmail.com>
2019-09-21 17:09:51 +0700

    Add help

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ if __name__ == "__main__":
         if (input_list[0] == "send"):
             with open(input_list[1], "rb") as binary_file:
                 send(binary_file.read(), input_list[2], port)
+        elif (input_list[0] == "help"):
+            print("Avaiable command : ")
+            print("-------------------")
+            print("send <file> <ip>")
         elif (input_list[0] == "quit"):
             break
         else:
